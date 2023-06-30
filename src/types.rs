0b15c954271125f3737d0c41832bab36845507d2
siddharthteli12 <telisiddharth123@yahoo.in>
2023-06-30 21:38:57 +0530

    Add: Default, validate for types.

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,5 @@
+const MAX_AMOUNT_OF_JOKES: i8 = 10;
+const MAX_ID_RANGE: i16 = 1368;
 pub enum Category {
     Chirstmas,
     Dark,
@@ -16,6 +18,12 @@ pub enum Language {
     Spanish,
 }
 
+impl Default for Language {
+    fn default() -> Self {
+        Language::English
+    }
+}
+
 pub enum Flags {
     Nsfw,
     Religious,
@@ -32,7 +40,52 @@ pub enum ResponseFormat {
     Text,
 }
 
+impl Default for ResponseFormat {
+    fn default() -> Self {
+        ResponseFormat::Json
+    }
+}
+
 pub enum JokeType {
+    Any,
     Single,
     TwoPart,
+}
+
+impl Default for JokeType {
+    fn default() -> Self {
+        JokeType::Any
+    }
+}
+
+pub struct AmountOfJokes(i8);
+
+impl AmountOfJokes {
+    pub fn validate(&self) {
+        if self.0 > 10 {
+            panic!(
+                "Max amount of jokes can be {} got {}",
+                MAX_AMOUNT_OF_JOKES, self.0
+            );
+        }
+    }
+}
+
+impl Default for AmountOfJokes {
+    fn default() -> Self {
+        Self(1)
+    }
+}
+
+pub struct IdRange(i16, i16);
+
+impl IdRange {
+    pub fn validate(&self) {
+        if self.0 > self.1 || self.1 > 1368 {
+            panic!(
+                "Unexpected id range value, max value allowed is {:}",
+                MAX_ID_RANGE
+            );
+        }
+    }
 }
